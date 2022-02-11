f63858dc75db5422c32e1977b9f91b2a9069b49f
nbenlin <nihatbenli.nb@gmail.com>
2022-02-11 17:47:07 +0100

    Add styling to Card component

--- src/components/UI/Card/Card.styled.js
+++ src/components/UI/Card/Card.styled.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const CardStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: #fff;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   border-radius: 10px;
-  width: 480px;
-  height: 360px;
-  margin: 0 auto;
+  width: 48rem;
+  height: 40rem;
+  margin: 5rem auto;
 `;
