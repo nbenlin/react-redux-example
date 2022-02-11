ce8d7c02255b2d17071398bb81c96837e16ce6e0
nbenlin <nihatbenli.nb@gmail.com>
2022-02-11 13:12:29 +0100

    Add create Homepage component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthenticationForm from "../components/Auth/AuthenticationForm";
+
+const HomePage = () => {
+  return (
+    <>
+      <AuthenticationForm />
+    </>
+  );
+};
+
+export default HomePage;
