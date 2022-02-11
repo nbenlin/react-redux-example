4f375bbdcf310ced158061fc27db990c83535bc2
nbenlin <nihatbenli.nb@gmail.com>
2022-02-11 19:18:43 +0100

    Add create UI-slice

---
+++ src/store/ui.slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: { tableIsVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.tableIsVisible = !state.tableIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice;
