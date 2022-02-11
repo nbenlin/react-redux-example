760c193a2e679247efe0592be310dd27dbf80551
nbenlin <nihatbenli.nb@gmail.com>
2022-02-11 19:29:01 +0100

    Add reducers to main reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth.slice";
+import uiReducer from "./ui.slice";
+import tableReducer from "./auth.slice";
 
 const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: { auth: authReducer, ui: uiReducer, table: tableReducer },
 });
 export default store;
