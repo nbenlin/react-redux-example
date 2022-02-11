f19aa91e0d40865c04a8982be9d4e9ea1b2e37fe
nbenlin <nihatbenli.nb@gmail.com>
2022-02-11 19:35:08 +0100

    Add fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTableData } from "./store/table.actions";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage";
 
 function App() {
+  const dispatch = useDispatch();
+  const displayTable = useSelector((state) => state.ui.tableIsVisible);
+
+  useEffect(() => {
+    dispatch(fetchTableData());
+  }, [dispatch]);
+
   return (
     <>
       <Header />
-      <HomePage />
+      <HomePage display={displayTable} />
     </>
   );
 }
