8306cc04dea88b838d002dba3c13a201c01e330f
nbenlin <nihatbenli.nb@gmail.com>
2022-02-11 13:12:53 +0100

    Add create AuthenticationForm component

---
+++ src/components/Auth/AuthenticationForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../UI/Card/Card";
+import { Form } from "./AuthenticationForm.styled";
+
+const AuthenticationForm = () => {
+  return (
+    <Card>
+      <Form>AuthenticationForm</Form>
+    </Card>
+  );
+};
+
+export default AuthenticationForm;
