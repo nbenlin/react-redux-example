3738c4ca154e465cbe83769077c95479ef8639b0
nbenlin <nihatbenli.nb@gmail.com>
2022-02-11 15:05:43 +0100

    Add create AuthenticationForm component

--- src/components/Auth/AuthenticationForm.js
+++ src/components/Auth/AuthenticationForm.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Card from "../UI/Card/Card";
-import { Form } from "./AuthenticationForm.styled";
+import {
+  Form,
+  InputGroup,
+  Label,
+  Input,
+  Button,
+} from "./AuthenticationForm.styled";
 
 const AuthenticationForm = () => {
   return (
     <Card>
-      <Form>AuthenticationForm</Form>
+      <Form>
+        <InputGroup>
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" />
+        </InputGroup>
+        <InputGroup>
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" />
+        </InputGroup>
+        <Button>Submit</Button>
+      </Form>
     </Card>
   );
 };
--- src/components/Auth/AuthenticationForm.styled.js
+++ src/components/Auth/AuthenticationForm.styled.js
@@ -2,5 +2,46 @@ import styled from "styled-components";
 
 export const Form = styled.form`
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  height: 28vw;
+  padding: 0 3rem;
+`;
+
+export const InputGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1.4rem;
+`;
+
+export const Label = styled.label`
+  font-size: 1.6rem;
+`;
+
+export const Input = styled.input`
+  width: 30rem;
+  padding: 1rem;
+  font-size: 1.6rem;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const Button = styled.button`
+  margin: 0 auto;
+  margin-top: 2rem;
+  width: 10rem;
+  padding: 1rem;
+  border: none;
+  border-radius: 1rem;
+  color: #37474f;
+  font-size: 1.6rem;
+  font-weight: 600;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
