ad9e6ce5e8103f287e7eaadf828521edaf89f976
nbenlin <nihatbenli.nb@gmail.com>
2022-02-11 15:05:51 +0100

    Add modify HomePage comp

--- src/pages/HomePage.styled.js
+++ src/pages/HomePage.styled.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const HomepageWrapper = styled.section`
   background-color: #fafafa;
-  height: 50vh;
-  padding: 20px 20px;
+  height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
