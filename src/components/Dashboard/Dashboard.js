5d5331008ecbd036ec419b8a48769835611f70fa
nbenlin <nihatbenli.nb@gmail.com>
2022-02-11 17:47:34 +0100

    Add create Dashboard compoentn

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,78 @@
 import React from "react";
+import Button from "../UI/Button/Button";
+import {
+  DashboardContainer,
+  DashboardHeader,
+  DashboardHeaderTitle,
+  DashboardHeaderButton,
+  DashboardBody,
+  Table,
+  THead,
+  Tr,
+  Th,
+  TBody,
+  Td,
+} from "./Dashboard.styled";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <DashboardContainer>
+      <DashboardHeader>
+        <DashboardHeaderTitle>User list</DashboardHeaderTitle>
+        <DashboardHeaderButton>Add new</DashboardHeaderButton>
+      </DashboardHeader>
+      <DashboardBody>
+        <Table cellPadding={0} cellSpacing={0}>
+          <THead>
+            <Tr>
+              <Th>Id</Th>
+              <Th>Name</Th>
+              <Th>Username</Th>
+              <Th>Email</Th>
+              <Th>City</Th>
+              <Th>Edit</Th>
+              <Th>Delete</Th>
+            </Tr>
+          </THead>
+          <TBody>
+            <Tr>
+              <Td>22</Td>
+              <Td>Nihat</Td>
+              <Td>Benli</Td>
+              <Td>nihatbenli.nb@gmail.com</Td>
+              <Td>Poznan</Td>
+              <Td>
+                <Button primary>edit</Button>
+              </Td>
+              <Td>deleteButton</Td>
+            </Tr>
+            <Tr>
+              <Td>52</Td>
+              <Td>Adam</Td>
+              <Td>Nowak</Td>
+              <Td>adamnowak@test.com</Td>
+              <Td>Wroclaw</Td>
+              <Td>
+                <Button primary>edit</Button>
+              </Td>
+              <Td>deleteButton</Td>
+            </Tr>
+            <Tr>
+              <Td>65</Td>
+              <Td>Jan</Td>
+              <Td>Kowalski</Td>
+              <Td>kowalski@test.com</Td>
+              <Td>Warszawa</Td>
+              <Td>
+                <Button primary>edit</Button>
+              </Td>
+              <Td>deleteButton</Td>
+            </Tr>
+          </TBody>
+        </Table>
+      </DashboardBody>
+    </DashboardContainer>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/Dashboard/Dashboard.styled.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const DashboardContainer = styled.section`
+  position: absolute;
+  top: 2rem;
+  box-shadow: 0px 0px 11px 0px rgba(66, 68, 90, 1);
+  border-radius: 1.2rem;
+  height: 80vh;
+`;
+
+export const DashboardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5rem;
+  border-bottom: 2px solid #bdbdbd;
+`;
+export const DashboardHeaderTitle = styled.h2`
+  font-size: 2rem;
+  font-weight: 600;
+`;
+export const DashboardHeaderButton = styled.button`
+  background-color: #1769aa;
+  border: none;
+  border-radius: 7px;
+  padding: 1rem 4rem;
+  font-size: 1.4rem;
+  font-weight: 600;
+  color: white;
+`;
+export const DashboardBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+
+  margin: 2rem;
+`;
+
+export const Table = styled.table`
+  width: auto;
+  height: 10vh;
+  overflow: hidden;
+  padding: 1px;
+  font-size: 1.4rem;
+  font-weight: 600;
+  border: 2px solid #e0e0e0;
+`;
+
+export const THead = styled.thead`
+  font-size: 1.6rem;
+  background-color: #e0e0e0;
+  padding: 3.5rem;
+`;
+
+export const Tr = styled.tr`
+  padding: 3.5rem;
+`;
+
+export const Th = styled.th`
+  padding: 3.5rem;
+`;
+
+export const TBody = styled.tbody`
+  padding: 3.5rem;
+`;
+
+export const Td = styled.td`
+  padding: 3.5rem;
+`;
